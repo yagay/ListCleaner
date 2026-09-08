3b1df13bc364cea0774975d5406e991d057748cb
Jie <1614094+yagay@users.noreply.github.com>
2026-09-08 06:40:48 +0100

    feat: localize component title dialog

--- app/src/main/java/com/yagay/ListCleaner/ui/ComponentTitleDialog.kt
+++ app/src/main/java/com/yagay/ListCleaner/ui/ComponentTitleDialog.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
